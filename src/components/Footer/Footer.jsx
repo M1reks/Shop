dcf9a487ef40f82d2b546b4d75e6b7c17cef5df0
Danilo <131854792+DaniloYarmolenko@users.noreply.github.com>
2024-01-06 21:35:51 +0200

    Create Footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import style from "./Footer.module.scss";
+
+const Footer = () => {
+  return (
+    <div className="container">
+      <div className={style.footer}>
+        <p className={style.text}>Copyright © 2024 «Name store» rights reserved</p>
+      </div>
+    </div>
+  )
+};
+
+export default Footer;
