6358602ad1ac4e24d43c5b229673f0becfdb6b56
Danilo <131854792+DaniloYarmolenko@users.noreply.github.com>
2024-01-24 16:10:19 +0200

    Crete card

--- src/app/RoutesData.js
+++ src/app/RoutesData.js
@@ -4,6 +4,7 @@ import { Routes } from "./routes.js";
 
 const LogInPage = lazy(() => import("../pages/LogInPage/LogInPage.jsx"));
 const SignInPage = lazy(() => import("../pages/SignInPage/SignInPage.jsx"));
+const MainMenuPage = lazy(() => import("../pages/MeinMenuPage/MainMenuPage.jsx"));
 
 export const RoutesData = [
   {
@@ -13,5 +14,9 @@ export const RoutesData = [
   {
     path: Routes.Login,
     element: SignInPage,
+  },
+  {
+    path: Routes.MainMenu,
+    element: MainMenuPage,
   },
 ];
