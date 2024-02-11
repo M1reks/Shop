e6f8e49ad23c8748fa59d926cbf5ce4d88884f13
Danya <rererr2008@gmail.com>
2024-02-11 18:27:08 +0200

    Update

--- src/components/MainMenu/components/FilterCatalog/FilterCatalog.jsx
+++ src/components/MainMenu/components/FilterCatalog/FilterCatalog.jsx
@@ -1,5 +1,11 @@
 import "rc-slider/assets/index.css";
 
+// const filter = [
+//   { title: "Бренд", value: ["Asus", "Apple", "Lenovo"] },
+//   { title: "Процесор", value: ["i5", "i7", "i9"] },
+//   { title: "Діагональ екрану", value: ["17", "23", "28"] },
+// ];
+
 const FilterCatalog = () => {
   return <div></div>;
 };
--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,17 @@ import "./styles/index.scss";
 
 import { BrowserRouter } from "react-router-dom";
 import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
 
-import "./i18n";
+import "./i18n.js";
 
 import App from "./App.jsx";
+import store from "./store";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </BrowserRouter>,
 );
